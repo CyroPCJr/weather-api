592bbac9ad103784cf9282168127b6fe3e24c4f6
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2025-05-08 10:24:45 -0300

    :rocket: first commit!

---
+++ src/main/kotlin/com/weatherworld/weather_api/WeatherApiApplication.kt
@@ -0,0 +1,11 @@
+package com.weatherworld.weather_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeatherApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WeatherApiApplication>(*args)
+}

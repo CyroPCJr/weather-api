9adabad81e44d2fa821d877b1af46da060f4bab4
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2025-05-28 23:02:28 -0300

    Update DotenvConfig.kt

--- src/main/kotlin/com/weatherworld/config/DotenvConfig.kt
+++ src/main/kotlin/com/weatherworld/config/DotenvConfig.kt
@@ -1,14 +1,28 @@
 package com.weatherworld.config
 
-import io.github.cdimascio.dotenv.dotenv
+import io.github.cdimascio.dotenv.Dotenv
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class DotenvConfig {
-    init {
-        val dotenv = dotenv()
-        dotenv.entries().forEach { entry ->
-            System.setProperty(entry.key, entry.value)
+    private val log = LoggerFactory.getLogger(DotenvConfig::class.java)
+
+    @PostConstruct
+    fun loadEnv() {
+        runCatching {
+            Dotenv
+                .configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load()
+                .entries()
+                .forEach { entry ->
+                    System.setProperty(entry.key, entry.value)
+                }
+        }.onFailure {
+            log.error("File .env not found - using system variables: ${it.message}")
         }
     }
 }
